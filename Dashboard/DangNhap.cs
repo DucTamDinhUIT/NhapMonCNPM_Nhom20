a3c5602c7b79d0ac2281937d7de7f37f74e6a473
DucTamDinhUIT <ductam.astro@gmail.com>
2020-06-27 16:04:09 +0700

    Update DangNhap.cs

--- Dashboard/DangNhap.cs
+++ Dashboard/DangNhap.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql;
+using MySql.Data.MySqlClient;
 
 namespace QuanLyCLBVoThuat
 {
@@ -17,8 +19,34 @@ namespace QuanLyCLBVoThuat
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Dang_Nhap(object sender, EventArgs e)
         {
+            //xac dinh duong dan den database
+
+            //ket noi csdl bang Sqlconnection
+
+            MySqlConnection conn = DBUtils.GetDBConnection();
+
+            try
+            {
+                //Mo ket noi
+                conn.Open();
+            }
+            catch (InvalidOperationException)
+            {
+                //xu ly khi ket noi co van de
+                MessageBox.Show("Khong the mo ket noi hoac ket noi da mo truoc do");
+            }
+            catch (Exception)
+            {
+                //xu ly khi ket noi co van de
+                MessageBox.Show("Ket noi xay ra loi hoac doc du lieu bi loi");
+            }
+            finally
+            {
+                //Dong ket noi sau khi thao tac ket thuc
+                conn.Close();
+            }
             DashboardControl dashboard = new DashboardControl();
                 dashboard.ShowDialog();
         }
