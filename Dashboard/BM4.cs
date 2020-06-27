513cf3c7c72696b775473ccad1d6e5a30259434b
DucTamDinhUIT <ductam.astro@gmail.com>
2020-06-27 13:55:41 +0700

    Update BM4.cs

--- Dashboard/BM4.cs
+++ Dashboard/BM4.cs
@@ -16,5 +16,15 @@ namespace QuanLyCLBVoThuat
         {
             InitializeComponent();
         }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
