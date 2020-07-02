a5fc2bdcc344f077bebe9d1ad1ffc2a840bbecec
DucTamDinhUIT <ductam.astro@gmail.com>
2020-07-02 12:19:24 +0700

    ned

--- Dashboard/BM1.cs
+++ Dashboard/BM1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace QuanLyCLBVoThuat
 {
@@ -17,9 +18,32 @@ namespace QuanLyCLBVoThuat
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
+            //SqlConnection con = new SqlConnection("Data Source=NiluNilesh;Integrated Security=True");  
+            //string connectionSTR = "Data Source=.\\sqlexpress;Initial Catalog=QuanLyCLB;Integrated Security=True";
 
+
+            SqlConnection con = new SqlConnection(@"Data Source =.\\sqlexpress; Initial Catalog = QuanLyCLB; Integrated Security = True");
+            SqlCommand cmd = new SqlCommand("sp_insert", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@STT", textBox6.Text);
+            cmd.Parameters.AddWithValue("@TenVoSinh", textBox1.Text);
+            cmd.Parameters.AddWithValue("@Truong", textBox2.Text);
+            cmd.Parameters.AddWithValue("@SinhNhat", textBox3.Text);
+            cmd.Parameters.AddWithValue("@CapBac", comboBox1.Text);
+            cmd.Parameters.AddWithValue("@NgayThamGia", textBox4.Text);
+
+
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+
+            con.Close();
+
+            if (i != 0)
+            {
+                MessageBox.Show(i + "Data Saved");
+            }
         }
     }
 }
