84995b3ce9ab1f7575488e0d5fd861d988ce0c3d
DucTamDinhUIT <ductam.astro@gmail.com>
2020-07-04 10:54:47 +0700

    Create SetInfor.cs

---
+++ Dashboard/SetInfor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+using QuanLyCLBVoThuat.DAO;
+using QuanLyCLBVoThuat.DTO;
+
+
+namespace QuanLyCLBVoThuat
+{
+    public partial class SetInfor : Form
+    {
+        public SetInfor()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+                string stt = textBox6.Text;
+                string tenvosinh = textBox2.Text;
+                string truong = textBox3.Text;
+            if (InformationDAO.Instance.InsertInfo(stt, tenvosinh, truong))
+            {
+                MessageBox.Show("Thêm thành công");
+                            }
+            else
+            {
+                MessageBox.Show("Có lỗi khi thêm");
+            }
+
+        }
+    }
+}
