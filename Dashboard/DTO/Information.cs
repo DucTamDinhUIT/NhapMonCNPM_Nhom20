c79c449a74f26de7860ab19324560166d32ed8af
DucTamDinhUIT <ductam.astro@gmail.com>
2020-07-04 10:54:36 +0700

    Create Information.cs

---
+++ Dashboard/DTO/Information.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace QuanLyCLBVoThuat.DTO
+{
+    public class Information
+    {
+        public Information(string stt, string tenvosinh, string truong)
+        {
+            this.STT = stt;
+            this.TenVoSinh = tenvosinh;
+            this.Truong = truong;
+
+        }
+
+        private string truong;
+
+        public string Truong
+        {
+            get { return truong; }
+            set { truong = value; }
+        }
+
+        private string tenvosinh;
+
+        public string TenVoSinh
+        {
+            get { return tenvosinh; }
+            set { tenvosinh = value; }
+        }
+
+        public string STT { get; set; }
+    }
+}
