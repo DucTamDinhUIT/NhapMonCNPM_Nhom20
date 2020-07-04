eb97d1f1549cc38b0ce256728b831a7e5093bbb4
DucTamDinhUIT <ductam.astro@gmail.com>
2020-07-05 04:35:57 +0700

    Update BM5_FindInfor.cs

--- Dashboard/GUI/BM5_FindInfor.cs
+++ Dashboard/GUI/BM5_FindInfor.cs
@@ -8,34 +8,45 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using QuanLyCLBVoThuat.DAO;
+using System.Data.SqlClient;
 
 namespace QuanLyCLBVoThuat
 {
     public partial class BM5_FindInfor : Form
     {
-
-        BindingSource vosinhList = new BindingSource();
-
         public BM5_FindInfor()
         {
             InitializeComponent();
+            FindInfor_Load();
         }
+        //string connecSTR = "Data Source=.\\sqlexpress;Initial Catalog=QuanLyCLB;Integrated Security=True";
 
+        SqlConnection conn = new SqlConnection(@"Data Source =.\\sqlexpress; Initial Catalog = QuanLyCLB; Integrated Security = True");
+       
+        void FindInfor_Load()
+        {
+            string query = "SELECT * FROM dbo.VoSinh ";
+            //conn.Open();
+            SqlCommand command = new SqlCommand(query, conn);
+            
+            DataTable data = new DataTable();
+
+            SqlDataAdapter adapter = new SqlDataAdapter(command); 
+
+            adapter.Fill(data);
+
+            //conn.Close();
+
+            dataGridView1.DataSource = data;
+
+        }
         private void TimKiem(object sender, EventArgs e)
         {
-            string hoTen = textBox1.Text;
-            string capBac = comboBox1.Text;
-                LoadTimKiem();
+            var dap = new SqlDataAdapter("SELECT * FROM dbo.VoSinh ", conn);
+            var table = new DataTable();
+            dap.Fill(table);
+            dataGridView1.DataSource = table;
         }
 
-        private void LoadTimKiem()
-        {
-            vosinhList.DataSource = AccountDAO.Instance.TimKiem();
-        }
-
-        DataTable TimKiem(string hoTen, string capBac)
-        {
-            return VoSinhDAO.Instance.TimKiem(hoTen, capBac);
-        }
     }
 }
