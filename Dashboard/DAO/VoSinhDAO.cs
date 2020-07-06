535e860fa9d6239461433442dcd05f91e921d148
DucTamDinhUIT <ductam.astro@gmail.com>
2020-07-06 11:49:10 +0700

    Update VoSinhDAO.cs

--- Dashboard/DAO/VoSinhDAO.cs
+++ Dashboard/DAO/VoSinhDAO.cs
@@ -18,9 +18,10 @@ namespace QuanLyCLBVoThuat.DAO
             private set { instance = value; }
         }
 
-        public bool InsertInfo(string stt, string tenvosinh, string truong)
+        public bool InsertInfo(string stt, string tenvosinh, string truong, string sinhnhat, string capbac, string ngaythamgia)
         {
-            string query = string.Format("INSERT INTO dbo.Information ( STT , TenVoSinh , Truong ) VALUES  ( N'{0}' , {1} , {2})" , stt , tenvosinh , truong );
+            //string query = "INSERT INTO dbo.Information ( STT , TenVoSinh , Truong ) VALUES  ( N'{0}' , {1} , {2})" , stt , tenvosinh , truong )";
+            string query = "INSERT INTO dbo.VoSinh (STT, TenVoSinh, Truong, SinhNhat, CapBac, NgayThamGia)  VALUES(' " + stt + " ', ' " + tenvosinh + " ', '  " + truong + " ', '" + sinhnhat + "', '" + capbac + "', '" + ngaythamgia + "')";
             int result = DataProvider.Instance.ExecuteNonQuery(query);
 
             return result > 0;
