66afda949c9ec37e380eef4066596d44e9cd1da8
DucTamDinhUIT <ductam.astro@gmail.com>
2020-07-06 11:49:22 +0700

    Update DashboardControl.Designer.cs

--- Dashboard/GUI/DashboardControl.Designer.cs
+++ Dashboard/GUI/DashboardControl.Designer.cs
@@ -34,7 +34,6 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button6
@@ -97,22 +96,11 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(11, 143);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(91, 34);
-            this.button7.TabIndex = 18;
-            this.button7.Text = "Nhập Thông tin võ sinh";
-            this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
-            // 
             // DashboardControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
@@ -133,6 +121,5 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button7;
     }
 }
