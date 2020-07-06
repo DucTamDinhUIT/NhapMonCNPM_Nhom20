3a88f64aa0cd2263437bd6140a3698b7594b1146
DucTamDinhUIT <ductam.astro@gmail.com>
2020-07-07 03:13:30 +0700

    Update BM2.cs

--- Dashboard/GUI/BM2.cs
+++ Dashboard/GUI/BM2.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using QuanLyCLBVoThuat.DAO;
+using QuanLyCLBVoThuat.DTO;
+using System.Configuration;
 
 namespace QuanLyCLBVoThuat
 {
@@ -17,5 +21,48 @@ namespace QuanLyCLBVoThuat
             InitializeComponent();
         }
 
+        private void Send_Click(object sender, EventArgs e)
+        {
+            /*
+            int stt = stt;
+            string tenvosinh = tenVoSinh.Text;
+            string truong = truongTh.Text;
+            string sinhnhat = sinhNhat.Text;
+            string capbac = capBac.Text;
+            string ngaythamgia = ngayThamGia.Text;
+
+            if (VoSinhDAO.Instance.InsertPrice(stt, tenvosinh, truong, sinhnhat, capbac, ngaythamgia))
+            {
+                MessageBox.Show("Thêm thành công");
+                SoThuTu_load();
+            }
+            else
+            {
+                MessageBox.Show("Có lỗi khi thêm");
+            }
+
+            dataShow.DataSource = GetData();
+        }
+
+        private DataTable GetData()
+        {
+            DataTable dataTable = new DataTable();
+            string connString = ConfigurationManager.ConnectionStrings["dbx"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connString))
+            {
+                //INSERT INTO dbo.VoSinh(STT, TenVoSinh, Truong, SinhNhat, CapBac, NgayThamGia)  VALUES('3', 'Thanh', 'UIT', 'Haiphong', 'trắng', '26/8');
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.VoSinh", con))
+                {
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    dataTable.Load(reader);
+                    con.Close();
+                };
+            }
+            return dataTable;
+            */
+        }
+        
     }
+
 }
