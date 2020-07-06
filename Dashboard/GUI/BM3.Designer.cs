da4feae8f25b0394b3ff77b939c37b91445193cb
DucTamDinhUIT <ductam.astro@gmail.com>
2020-07-07 06:39:07 +0700

    Update BM3.Designer.cs

--- Dashboard/GUI/BM3.Designer.cs
+++ Dashboard/GUI/BM3.Designer.cs
@@ -28,57 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Stt = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Event = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Prices = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.CreatDay = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Notes = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.phieuThuChi = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.phieuThuChi)).BeginInit();
             this.SuspendLayout();
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Stt,
-            this.Event,
-            this.Prices,
-            this.CreatDay,
-            this.Notes});
-            this.dataGridView1.Location = new System.Drawing.Point(1, 71);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(797, 367);
-            this.dataGridView1.TabIndex = 0;
-            // 
-            // Stt
-            // 
-            this.Stt.HeaderText = "Số thứ tự";
-            this.Stt.Name = "Stt";
-            // 
-            // Event
-            // 
-            this.Event.HeaderText = "Nội dung thu/chi";
-            this.Event.Name = "Event";
-            // 
-            // Prices
-            // 
-            this.Prices.HeaderText = "Số tiền";
-            this.Prices.Name = "Prices";
-            // 
-            // CreatDay
-            // 
-            this.CreatDay.HeaderText = "Ngày lập phiếu";
-            this.CreatDay.Name = "CreatDay";
-            // 
-            // Notes
-            // 
-            this.Notes.HeaderText = "Ghi chú";
-            this.Notes.Name = "Notes";
             // 
             // button1
             // 
@@ -88,6 +43,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Search";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Search_Click);
             // 
             // textBox1
             // 
@@ -105,33 +61,36 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "Tìm kiếm";
             // 
+            // phieuThuChi
+            // 
+            this.phieuThuChi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.phieuThuChi.Location = new System.Drawing.Point(27, 73);
+            this.phieuThuChi.Name = "phieuThuChi";
+            this.phieuThuChi.Size = new System.Drawing.Size(729, 358);
+            this.phieuThuChi.TabIndex = 4;
+            // 
             // BM3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.phieuThuChi);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
             this.Name = "BM3";
             this.Text = "Tra cứu thu chi ngân quỹ";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.BM3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.phieuThuChi)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Stt;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Event;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Prices;
-        private System.Windows.Forms.DataGridViewTextBoxColumn CreatDay;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Notes;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView phieuThuChi;
     }
 }
