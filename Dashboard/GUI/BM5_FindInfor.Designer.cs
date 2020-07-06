e344c700290d8fb9024e7f10faeeccb1dc23fbe7
DucTamDinhUIT <ductam.astro@gmail.com>
2020-07-07 06:39:18 +0700

    Update BM5_FindInfor.Designer.cs

--- Dashboard/GUI/BM5_FindInfor.Designer.cs
+++ Dashboard/GUI/BM5_FindInfor.Designer.cs
@@ -29,20 +29,20 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.capbac1 = new System.Windows.Forms.ComboBox();
+            this.searchText = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.bShowAll = new System.Windows.Forms.Button();
             this.dataVoSinh = new System.Windows.Forms.DataGridView();
             this.dataSet1 = new System.Data.DataSet();
+            this.Delete = new System.Windows.Forms.Button();
+            this.Edit = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataVoSinh)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(493, 25);
+            this.button1.Location = new System.Drawing.Point(346, 25);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(175, 26);
             this.button1.TabIndex = 1;
@@ -50,27 +50,12 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.TimKiem);
             // 
-            // textBox1
+            // searchText
             // 
-            this.textBox1.Location = new System.Drawing.Point(40, 29);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(277, 20);
-            this.textBox1.TabIndex = 2;
-            // 
-            // capbac1
-            // 
-            this.capbac1.AutoCompleteCustomSource.AddRange(new string[] {
-            "Tên võ sinh",
-            "Mã võ sinh",
-            "Trường học",
-            "Ngày sinh",
-            "Cấp bậc",
-            "Ngày tham gia"});
-            this.capbac1.FormattingEnabled = true;
-            this.capbac1.Location = new System.Drawing.Point(356, 29);
-            this.capbac1.Name = "capbac1";
-            this.capbac1.Size = new System.Drawing.Size(121, 21);
-            this.capbac1.TabIndex = 3;
+            this.searchText.Location = new System.Drawing.Point(40, 29);
+            this.searchText.Name = "searchText";
+            this.searchText.Size = new System.Drawing.Size(277, 20);
+            this.searchText.TabIndex = 2;
             // 
             // label1
             // 
@@ -81,48 +66,59 @@
             this.label1.TabIndex = 4;
             this.label1.Text = "Ký tự";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(397, 10);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(27, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Loại";
-            // 
             // bShowAll
             // 
-            this.bShowAll.Location = new System.Drawing.Point(690, 25);
+            this.bShowAll.Location = new System.Drawing.Point(539, 25);
             this.bShowAll.Name = "bShowAll";
-            this.bShowAll.Size = new System.Drawing.Size(98, 26);
+            this.bShowAll.Size = new System.Drawing.Size(131, 26);
             this.bShowAll.TabIndex = 6;
             this.bShowAll.Text = "Xem tất cả";
             this.bShowAll.UseVisualStyleBackColor = true;
             this.bShowAll.Click += new System.EventHandler(this.BShowAll_Click);
             // 
-            // dataGridView1
+            // dataVoSinh
             // 
             this.dataVoSinh.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataVoSinh.Location = new System.Drawing.Point(49, 74);
-            this.dataVoSinh.Name = "dataGridView1";
-            this.dataVoSinh.Size = new System.Drawing.Size(721, 335);
+            this.dataVoSinh.Location = new System.Drawing.Point(49, 118);
+            this.dataVoSinh.Name = "dataVoSinh";
+            this.dataVoSinh.Size = new System.Drawing.Size(721, 291);
             this.dataVoSinh.TabIndex = 7;
             // 
             // dataSet1
             // 
             this.dataSet1.DataSetName = "NewDataSet";
             // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(346, 73);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(175, 26);
+            this.Delete.TabIndex = 8;
+            this.Delete.Text = "Xoá";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // Edit
+            // 
+            this.Edit.Location = new System.Drawing.Point(539, 73);
+            this.Edit.Name = "Edit";
+            this.Edit.Size = new System.Drawing.Size(131, 26);
+            this.Edit.TabIndex = 9;
+            this.Edit.Text = "Sửa";
+            this.Edit.UseVisualStyleBackColor = true;
+            this.Edit.Click += new System.EventHandler(this.Edit_Click);
+            // 
             // BM5_FindInfor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Edit);
+            this.Controls.Add(this.Delete);
             this.Controls.Add(this.dataVoSinh);
             this.Controls.Add(this.bShowAll);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.capbac1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.searchText);
             this.Controls.Add(this.button1);
             this.Name = "BM5_FindInfor";
             this.Text = "Tìm kiếm danh sách võ sinh";
@@ -135,12 +131,12 @@
 
         #endregion
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ComboBox capbac1;
+        private System.Windows.Forms.TextBox searchText;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button bShowAll;
         private System.Windows.Forms.DataGridView dataVoSinh;
         private System.Data.DataSet dataSet1;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button Edit;
     }
 }
